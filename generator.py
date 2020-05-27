0f047dbd0e8e3c7bcb68f0e47ccfcfe32180c439
dkimpara <dkimpara3@gatech.edu>
2020-05-27 18:54:49 +0700

    installing wsl2

--- generator.py
+++ generator.py
@@ -1,26 +1,16 @@
 ## generate graphs
 
-import graph_tool.all
+import networkx as nx
+from networkx.generators.community import stochastic_block_model
 import numpy as np
-import itertools
-import matplotlib.pyplot as plt
-import random
-import numpy as np
-import copy
-from mpl_toolkits import mplot3d
-from multiprocessing import Pool, TimeoutError
 
-def graph_gen():
-#num_groups, num_nodes, intra_edge_density, extra_edge_density):
-    '''num_groups is number of nodes in each group
-    num_nodes
-        uses SBM as graph generator'''
-    g, bm = graph_tool.generation.random_graph(50, poisson(2),
-                                    directed=True,
-                                    model="blockmodel",
-                                    block_membership=membership,
-                                    edge_probs=prob)
-    graph_tool.graph_draw(g, vertex_fill_color=bm, edge_color="black", output="blockmodel.pdf")
+def graph_gen(num_groups, num_nodes, intra_edge_density, extra_edge_density):
+    """num_groups is number of nodes in each group
+    num_nodes uses SBM as graph generator"""
+    sizes = [int(num_nodes/num_groups)] * 2
+    p = generate_density_matrix(intra_edge_density, extra_edge_density)
+    
+    g = stochastic_block_model(sizes, p, directed=True, selfloops=False)
     return g
 
 def culture_init(g, distance, std_dev):
@@ -31,12 +21,13 @@ def edge_weight_init(g, distribution='uniform'):
     
     return g
 
-def prob(a,b):
-    '''probability function for edge density between groups'''
-    if a == b: return 0.2
-    else: return 0.02
-def membership(a):
-    if a < 25: return 0
-    else: return 1
+
+# noinspection PyStatementEffect
+def generate_density_matrix(intra_edge_density, extra_edge_density):
+    assert isinstance(intra_edge_density, float)
+    assert isinstance(extra_edge_density, float)
+    intra = intra_edge_density
+    out = extra_edge_density
+    return [[intra, out],[out, intra]]
 
         
