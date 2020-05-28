f37b64e04c3502597816666331dd0b2c296eeabc
dkimpara <dkimpara3@gatech.edu>
2020-05-28 14:47:27 +0700

    tested random_perturb_culture

--- generator.py
+++ generator.py
@@ -14,17 +14,17 @@ def graph_gen(num_groups, num_nodes, intra_edge_density, extra_edge_density):
     g = stochastic_block_model(sizes, p, directed=True, selfloops=False)
     return g
 
-def culture_init(g, std_dev, change_vec, dim=10, distance=3.0):
+def culture_init(g, std_dev, change_vec, dim=10, distance=3.0, norm_p=2):
     """change_vec = [[d1,r1][d2,r2]]"""
     culture1 = random.rand(dim)
-    culture2 = culture1 + sample_n_sphere_surface(dim, change_vec, distance)
+    culture2 = culture1 + random_perturb_culture(dim, change_vec, distance)
 
     culture1 = np.append(culture1, change_vec[0])
     culture2 = np.append(culture2, change_vec[1])
 
     #now init cultures for each node
 
-    print(linalg.norm(culture2-culture1, 2)) #check norm worked
+    
     return g
 
 def random_perturb_culture(ndim, change_vec, distance, norm_p=2):
@@ -33,22 +33,24 @@ def random_perturb_culture(ndim, change_vec, distance, norm_p=2):
 
     vec = sample_n_sphere_surface(ndim, norm_p)
 
-    diff1 = abs(change_vec[0, 0]-change_vec[0, 1])
-    diff2 = abs(change_vec[1, 0]-change_vec[1, 1])
-    vec *= np.power(distance ** norm_p - diff1 ** norm_p - diff2 ** norm_p,
+    diff1 = abs(change_vec[0][0]-change_vec[1][0])
+    diff2 = abs(change_vec[0][1]-change_vec[1][1])
+    vec *= np.power(np.power(distance, norm_p)
+                    - np.power(diff1, norm_p)
+                    - np.power(diff2, norm_p),
                     1.0/norm_p)
     return vec
 
 def sample_n_sphere_surface(ndim, norm_p=2):
     """sample random vector from S^n-1 with norm_p"""
 
-    vec = random.randn(ndim) #random vector to put distance between cultures
+    vec = random.randn(ndim) #random ve ctor to put distance between cultures
     vec = vec / linalg.norm(vec, norm_p) #create random vector with norm 1
     return vec
 
 def edge_weight_init(g, distribution='uniform'):
     for edge in g.edges():
-        edges[edge]['weight'] = random.uniform(0.1)
+        g.edges[edge]['weight'] = random.uniform(0.1)
     return g
 
 # noinspection PyStatementEffect
---
+++ tests.py
@@ -0,0 +1,7 @@
+import generator as gen
+avg = 0
+for i in range(500):
+    d = gen.culture_init(0, 0, [[0.5,0.5],[1.0,1.0]])
+    avg += d
+    print(d)
+print(avg/500)
