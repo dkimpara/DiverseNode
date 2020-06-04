a68e281cde27e7e03ed7bfafc0769f4bc22a3b76
dkimpara <dhammakimpara@gmail.com>
2020-06-04 15:10:55 +0700

    started sim code, laptop

--- simulate.py
+++ simulate.py
@@ -1,15 +1,43 @@
 #simulate dynamics on graph
 
+#global connected component class?
+#optimize by making g,culturemat global?
+import random
 
 def simulate_iterstop(g, iter=500):
     '''input graph g with initialized culture state, edge weights,'''
 
     for i in range(iter):
-        g = sim_one_iter(g)
+        g, culturemat = sim_one_iter(g, culturemat)
 
     #write adj matrix and culture vec to file
     return g #for prototyping purposes.
 
-def sim_one_iter(g):
+def sim_one_iter(g, culturemat):
+	
+	#determine random sequence of individuals
+	nodeList = list(g.nodes)
+	random.shuffle(nodeList)
+	
+	for u in nodeList:
+		#pick interaction randomly
+		v  = pick_interaction(u) #with connected component class
+		
+		#carry out interaction
+		prob_accept = p_accept(culturemat[u],culturemat[v])
+		
+		if random.random() < prob_accept:
+			#accept culture
+			#add node?
+		else:
+			#reject culture
+			#check for node to remove
+	
+    return g, culturemat
 
-    return g
+def pick_interaction(u):
+	v = 1
+	return v
+def p_accept(culture_u, culture_v):
+	prob = 1
+	return prob
