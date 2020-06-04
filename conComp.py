7b14008b72c97aa445fc23908a1c288945acd962
dkimpara <dkimpara3@gatech.edu>
2020-06-04 21:01:16 +0700

    finished conComp, need to test, dktp

--- conComp.py
+++ conComp.py
@@ -1,24 +1,29 @@
 ##define object to keep track of connected components
+# upgrade: disjoint union find implementation use nx implementation
 
 import networkx as nx
+from networkx.utils.union_find import UnionFind
+
 
 class Components:
     def __init__(self, g):
-        g = nx.DiGraph.to_undirected(g) #shallow copy is fine
-        self.list_sets = list(nx.algorithms.components.connected_components(g))
-		
-		print(t)
-     
-    def merge(self, c1, c2):
-				
+        """
+
+        :rtype: nxgraph
+        """
+        self.uf = UnionFind() #init uf datastruct
+
+        g = nx.DiGraph.to_undirected(g)  # shallow copy is fine
+        for c in nx.algorithms.components.connected_components(g):
+            self.uf.union(*c)
+
+    def merge(self, u, v):
+        self.uf.union(u, v)
+
     def split(self, g):
-		g = nx.DiGraph.to_undirected(g) #shallow copy is fine
-		self.list_sets = list(nx.algorithms.components.connected_components(g))
-	
+        """reinit for now, optimize later"""
+        self.__init__(g)
+
     def find_component(self, u):
-		"""returns connected component of u as set"""
-		for c in self.gen_sets:
-			if u in c: return c
-		
-		raise ValueError('connected component not found')
-			
+        """returns connected component of u as set"""
+        return self.uf[u]
