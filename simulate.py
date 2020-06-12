52a7b543a55a231d661a657fddc2efde705d4b2f
dkimpara <dkimpara3@gatech.edu>
2020-06-12 18:42:13 +0700

    dealing with overflow warning in progress need to fix out of ranges for change vec stds

--- simulate.py
+++ simulate.py
@@ -79,8 +79,14 @@ def p_accept(culture_u, culture_v, culture_change_all, norm_p=2):
         dist = linalg.norm(culture_u - culture_v, norm_p)
     else:  # only original culture vec change (as in sayama)
         dist = linalg.norm(culture_u[:-3] - culture_v[:-3], norm_p)
-
-    return 0.5 ** (dist / d)
+    z = dist / d
+    ## todo: overflow encountered in double_scalars
+    try:
+        res = 0.5 ** z
+        return res
+    except FloatingPointError:
+        print(dist, d, z)
+        return 0.5
 
 
 def update_culture(node, other_node, culturemat, culture_change_all):
