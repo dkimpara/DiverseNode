427374b51dd18b88d79706d16803d68909d96433
dkimpara <dhammakimpara@gmail.com>
2020-07-08 19:41:08 +0700

    simulate code is generic

--- generator.py
+++ generator.py
@@ -6,7 +6,7 @@ from networkx.generators.community import stochastic_block_model
 import numpy as np
 from numpy import linalg
 
-
+# todo: modify for different g
 def graph_gen(num_groups, num_nodes, intra_edge_density, extra_edge_density):
     """num_groups is number of nodes in each group
     num_nodes uses SBM as graph generator
@@ -19,12 +19,12 @@ def graph_gen(num_groups, num_nodes, intra_edge_density, extra_edge_density):
     g = edge_weight_init(g)
     return g
 
-
+# todo: integrate to extract original culture
 def culture_init(g, std_devs, change_vec, dim=10, distance=3.0, norm_p=2):
     """change_vec = [[d1,r1,w1][d2,r2,w2]]
         std_devs = [sd_culture1, sd_tolerance1, sd_culture_change1, sd w1],[cult2"""
     culture1 = np.random.rand(dim)
-    culture2 = culture1 + random_perturb_culture(dim, change_vec, distance)
+    culture2 = culture1 + random_perturb_culture(dim, change_vec, distance, norm_p)
 
     culture1 = np.append(culture1, change_vec[0])
     culture2 = np.append(culture2, change_vec[1])
@@ -39,7 +39,7 @@ def culture_init(g, std_devs, change_vec, dim=10, distance=3.0, norm_p=2):
         elif b == 1:
             culturemat[v] = generate_node_culture(culture2, std_devs[1])
 
-    return culturemat
+    return culturemat, culture1, culture2
 
 
 def generate_node_culture(culture_center, s_devs):
